9991903f387847d1a19a54f7893460de374b2972
abhishek devop <standup787059@gmail.com>
2025-08-08 22:34:34 +0530

    basic setup

---
+++ src/routes/index.routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+
+router.get('/',(req,res)=>{
+     res.json({
+          message: 'hello from router...'
+     })
+})
+
+module.exports = router
