15d5f0da300e027b9f2432f53ae3f4fc491c86c1
abhishek devop <standup787059@gmail.com>
2025-08-11 00:28:47 +0530

    validation, cookie

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -14,10 +14,13 @@ router.post('/register', async (req,res)=>{
           id:user._id,
 
      },process.env.JWT_SECRET)
+
+     res.cookie("token", token)
+
      res.status(201).json({
           message:'user registered...',
           user,
-          token
+          
      })
 })
 
@@ -46,4 +49,33 @@ router.post('/login', async (req,res)=>{
      })
 })
 
+
+router.get('/user', async (req,res)=>{
+     const {token} = req.cookies;
+     if(!token){
+          return res.status(401).json({
+               message: "Unauthorized!"
+          })
+     }
+     try{
+
+          const decoded =  jwt.verify(token, process.env.JWT_SECRET)
+
+          const user = await userModel.findOne({
+               _id:decoded.id
+          }).select("-password")
+
+
+          res.status(200).json({
+               message:"user data fetched successfully...",
+               user
+          })
+     }catch(err){
+
+          return res.status(401).json({
+               message:'unauthorized - Invalid token'
+
+          })
+     }
+})
 module.exports = router
